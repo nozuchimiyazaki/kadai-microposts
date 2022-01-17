32b8b759ce4cf43e5b35c662a069de1468a4b7ef
nozuchi.miyazaki <user01.kazu+git@gmail.com>
2022-01-17 07:08:48 +0000

    user login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="center jumbotron">
-        <div class="text-center">
-            <h1>Welcome to the Microposts</h1>
-            {{-- ユーザ登録へのリンク --}}
-            {!! link_to_route('signup.get', 'Sign up now!',[], ['class' => 'btn btn-lb btn-primary']) !!}
+    @if (Auth::check())
+        {{ Auth::user()->name }}
+    @else
+        <div class="center jumbotron">
+            <div class="text-center">
+                <h1>Welcome to the Microposts</h1>
+                {{-- ユーザ登録へのリンク --}}
+                {!! link_to_route('signup.get', 'Sign up now!',[], ['class' => 'btn btn-lb btn-primary']) !!}
+            </div>
         </div>
-    </div>
+    @endif
 @endsection
