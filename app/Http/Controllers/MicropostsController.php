8b774ec23a0aed4384cf95faa040484e11836eb6
nozuchi.miyazaki <user01.kazu+git@gmail.com>
2022-01-23 03:12:54 +0000

    timeline

--- app/Http/Controllers/MicropostsController.php
+++ app/Http/Controllers/MicropostsController.php
@@ -12,9 +12,9 @@ class MicropostsController extends Controller
         if (\Auth::check()) {   // 認証済みの場合
             // 認証済みユーザを取得
             $user = \Auth::user();
-            // ユーザ投稿の一覧を作成日時の降順で取得
+            // ユーザとフォロー中ユーザの投稿の一覧を作成日時の降順で取得
             // （後のChapterで他ユーザの投稿も取得できるように変更
-            $microposts = $user->microposts()->orderBy('created_at', 'desc')->paginate(10);
+            $microposts = $user->feed_microposts()->orderBy('created_at', 'desc')->paginate(10);
             
             $data = [
                 'user' => $user,
